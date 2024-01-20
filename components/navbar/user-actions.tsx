14a544da8d53ce6feab684d061c794f1c7a9dcdb
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-20 14:05:35 +0530

    Add wallet connection and disconnection functionality to user actions.

--- components/navbar/user-actions.tsx
+++ components/navbar/user-actions.tsx
@@ -8,6 +8,8 @@ import {
 } from "~/components/ui/dropdown-menu";
 import { trpc } from "~/app/_trpc/client";
 import { signOut } from "next-auth/react";
+import { useCallback } from "react";
+import { useMbWallet } from "@mintbase-js/react";
 
 interface IProps {
   children: React.ReactNode;
@@ -15,19 +17,52 @@ interface IProps {
 
 export function UserActions({ children }: IProps) {
   const { data: user } = trpc.user.getAuthProfile.useQuery();
+  const { isConnected, selector, connect, activeAccountId } = useMbWallet();
+
+  const handleConnectWallet = useCallback(async () => {
+    await connect();
+    console.log(activeAccountId);
+  }, [connect, activeAccountId]);
+
+  const handleDisconnectWallet = useCallback(async () => {
+    const wallet = await selector.wallet();
+    return wallet.signOut();
+  }, [selector]);
 
   if (!user) return null;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          <div className="text-center">
+            {isConnected ? (
+              <>
+                <p className="font-light">Active wallet:</p>
+                <p>{activeAccountId}</p>
+              </>
+            ) : (
+              <p className="text-center text-xs font-light text-muted-foreground">
+                No wallet connected
+              </p>
+            )}
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Connect Wallet</DropdownMenuItem>
+        {!isConnected && (
+          <DropdownMenuItem onClick={handleConnectWallet}>
+            Connect Wallet
+          </DropdownMenuItem>
+        )}
         <DropdownMenuItem>Billing</DropdownMenuItem>
         <DropdownMenuItem>Team</DropdownMenuItem>
         <DropdownMenuItem>Subscription</DropdownMenuItem>
         <DropdownMenuSeparator />
+        {isConnected && (
+          <DropdownMenuItem onClick={handleDisconnectWallet}>
+            Disconnect Wallet
+          </DropdownMenuItem>
+        )}
         <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
