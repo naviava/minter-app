87e403144482dfc20c0e425a29834f97b168d56b
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-25 01:12:09 +0530

    Added redirect if token is not published.

--- app/(root)/nft/[tokenId]/page.tsx
+++ app/(root)/nft/[tokenId]/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from "next/navigation";
+
 import { PageWrapper } from "~/components/page-wrapper";
-import { serverClient } from "~/app/_trpc/server-client";
 import { TokenArticle } from "./_components/token-article";
+
+import { serverClient } from "~/app/_trpc/server-client";
 
 interface IProps {
   params: {
@@ -10,6 +13,7 @@ interface IProps {
 
 export default async function TokenIdPage({ params }: IProps) {
   const token = await serverClient.nft.getTokenById(params.tokenId);
+  if (!token.isPublished) return redirect("/explore");
 
   return (
     <PageWrapper>
