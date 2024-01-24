ef6214b0b690d24dd9b6774f29c33c6a3d2db9a0
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-25 00:20:35 +0530

    Improved AuthModal UX.

--- components/modals/auth-modal.tsx
+++ components/modals/auth-modal.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useCallback, useState } from "react";
+import { usePathname } from "next/navigation";
+
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
+import { DNA } from "react-loader-spinner";
 
 import { useAuthModal } from "~/store/use-auth-modal";
 import { useIsMounted } from "~/hooks/use-is-mounted";
@@ -17,8 +21,18 @@ import {
 import { Button } from "~/components/ui/button";
 
 export function AuthModal() {
+  const pathname = usePathname();
   const isMounted = useIsMounted();
+  const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onClose } = useAuthModal();
+
+  const handleClick = useCallback(
+    (provider: string) => {
+      setIsLoading(true);
+      signIn(provider, { callbackUrl: pathname });
+    },
+    [pathname],
+  );
 
   if (!isMounted) return null;
   return (
@@ -31,14 +45,20 @@ export function AuthModal() {
           </DialogDescription>
         </DialogHeader>
         <div className="mx-auto space-x-4">
-          <Button variant="outline" onClick={() => signIn("google")}>
-            <FcGoogle className="mr-2" />
-            Google
-          </Button>
-          <Button variant="outline" onClick={() => signIn("github")}>
-            <FaGithub className="mr-2" />
-            GitHub
-          </Button>
+          {isLoading ? (
+            <DNA />
+          ) : (
+            <>
+              <Button variant="outline" onClick={() => handleClick("google")}>
+                <FcGoogle className="mr-2" />
+                Google
+              </Button>
+              <Button variant="outline" onClick={() => handleClick("github")}>
+                <FaGithub className="mr-2" />
+                GitHub
+              </Button>
+            </>
+          )}
         </div>
       </DialogContent>
     </Dialog>
