81f41ffcb51b7a1822edc8beba54f07930db7abf
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-24 18:43:04 +0530

    Added hover tooltips for nft card actions.

--- components/hover-tip.tsx
+++ components/hover-tip.tsx
@@ -4,18 +4,30 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
+import { cn } from "~/lib/utils";
 
 interface IProps {
   children: React.ReactNode;
   message: string;
+  className?: string;
+  side?: "top" | "right" | "bottom" | "left";
+  sideOffset?: number;
+  delayDuration?: number;
 }
 
-export function HoverTip({ children, message }: IProps) {
+export function HoverTip({
+  children,
+  message,
+  className,
+  side = "bottom",
+  sideOffset = 10,
+  delayDuration = 300,
+}: IProps) {
   return (
-    <TooltipProvider>
+    <TooltipProvider delayDuration={delayDuration}>
       <Tooltip>
-        <TooltipTrigger>{children}</TooltipTrigger>
-        <TooltipContent>
+        <TooltipTrigger className={cn(className)}>{children}</TooltipTrigger>
+        <TooltipContent side={side} sideOffset={sideOffset}>
           <p>{message}</p>
         </TooltipContent>
       </Tooltip>
--- components/nft-card/nft-card-actions.tsx
+++ components/nft-card/nft-card-actions.tsx
@@ -4,6 +4,7 @@ import { RiHeartLine } from "react-icons/ri";
 import { ExternalLink, Eye } from "lucide-react";
 
 import { Separator } from "~/components/ui/separator";
+import { HoverTip } from "../hover-tip";
 
 interface IProps {
   id: string;
@@ -19,29 +20,35 @@ export function NftCardActions({ id }: IProps) {
 
   return (
     <div className="flex items-center gap-x-2">
-      <div
-        role="button"
-        className="flex flex-1 items-center justify-center transition hover:opacity-70"
-      >
-        <RiHeartLine className="h-[22px] w-[22px]" />
-      </div>
+      <HoverTip message="Add to favorites" className="flex-1">
+        <div
+          role="button"
+          className="flex items-center justify-center transition hover:opacity-70"
+        >
+          <RiHeartLine className="h-[22px] w-[22px]" />
+        </div>
+      </HoverTip>
       <Separator orientation="vertical" className="h-6" />
-      <a
-        href={`/nft/${id}`}
-        target="_blank"
-        className="flex flex-1 items-center justify-center transition hover:opacity-70"
-      >
-        <Eye className="h-[22px] w-[22px]" />
-      </a>
+      <HoverTip message="View this token" className="flex-1">
+        <a
+          href={`/nft/${id}`}
+          target="_blank"
+          className="flex items-center justify-center transition hover:opacity-70"
+        >
+          <Eye className="h-[22px] w-[22px]" />
+        </a>
+      </HoverTip>
       <Separator orientation="vertical" className="h-6" />
-      <div
-        role="button"
-        // TODO: Create share modal.
-        onClick={handleCopyLink}
-        className="flex flex-1 items-center justify-center transition hover:opacity-70"
-      >
-        <ExternalLink className="h-[22px] w-[22px]" />
-      </div>
+      <HoverTip message="Share" className="flex-1">
+        <div
+          role="button"
+          // TODO: Create share modal.
+          onClick={handleCopyLink}
+          className="flex items-center justify-center transition hover:opacity-70"
+        >
+          <ExternalLink className="h-[22px] w-[22px]" />
+        </div>
+      </HoverTip>
     </div>
   );
 }
