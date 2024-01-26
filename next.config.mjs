591e12c8275a97f6b4510a8a73ad06db766a6b93
Navin Avadhani <knavinavadhani@gmail.com>
2024-01-26 12:51:34 +0530

    CONFIG: Disabled PWA in dev mode and added postinstall script for prisma.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
